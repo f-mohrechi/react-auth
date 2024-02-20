12fe90b42ef25468a73feb8825a175693fc322b8
f-mohrechi <f.mohrechi78@gmail.com>
2024-02-20 23:51:04 +0330

    created 404 error page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/login";
 import Register from "./pages/register";
+import Error from "./pages/error";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/*" element={<Error />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/error.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Error = () => {
+  return (
+    <div className="h-screen bg-gray-800 flex flex-col justify-center items-center">
+      <p className="text-6xl text-sky-100 font-bold">404</p>
+      <p className="text-4xl text-sky-100 font-bold pt-3">Page not found!</p>
+    </div>
+  );
+};
+
+export default Error;
