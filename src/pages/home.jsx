5dbbf5a39cdd304198b8493bd4f96d78f3ca0777
f-mohrechi <f.mohrechi78@gmail.com>
2024-02-22 11:43:15 +0330

    access to home with loging in

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,9 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router";
 
 export default function Home() {
+  const navigate = useNavigate();
+  const [user, setUser] = useState({});
+
+  useEffect(() => {
+    const user = localStorage.getItem("user");
+
+    if (user) {
+      setUser(JSON.parse(user));
+    } else {
+      navigate("/login");
+    }
+  }, []);
+
   return (
     <div>
       <p>home</p>
+
+      {user && (
+        <>
+          <p>welcome {user.username}</p>
+        </>
+      )}
     </div>
   );
 }
